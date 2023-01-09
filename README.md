65438eff5774bb2faf738c4e809127d686722113
chunryanqian <chunryanqian@users.noreply.github.com>
2023-01-09 13:50:54 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# WindTable
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/chrome-dfs5zu)
---
+++ app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'navBar',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+  name = 'Wind Talker';
+}
